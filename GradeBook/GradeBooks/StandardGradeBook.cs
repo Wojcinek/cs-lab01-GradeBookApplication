ffdff709e0a819197a1abf7e6cdc3ca0c7a06fa6
Wojciech Cygan <Wojciech.Cygan@microsoft.wsei.edu.pl>
2022-04-03 21:11:06 +0200

    StandardGradebook

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class GradeBook.GradeBooks
+{
+	public StandardGradeBook : BaseGradeBook()
+	{
+	public StandardGradeBook(string name) : base(name)
+		{
+		Type = Enums.GradeBookType.Standard;
+		}
+	}
+}
