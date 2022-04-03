7cc970c134e92fcda00b660469f60e5d9851b723
Wojciech Cygan <Wojciech.Cygan@microsoft.wsei.edu.pl>
2022-04-03 21:24:08 +0200

    override Calculate studnet statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -39,5 +39,13 @@ namespace GradeBook.GradeBooks
             else if (StudentCount() >= 5)
                 base.CalculateStatistics();
         }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (StudentCount() < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else if (StudentCount() >= 5)
+                base.CalculateStudentStatistics(name);
+        }
     }
 }
